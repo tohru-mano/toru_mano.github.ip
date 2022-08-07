7b6dce21d0d0ff2f452aab6c5e584bf0446fd4c1
Toru_Mano <108441677+tohru-mano@users.noreply.github.com>
2022-08-07 23:40:52 +0900

    Update wrs.js

--- wrs.js
+++ wrs.js
@@ -1,6 +1,10 @@
 'use strict'
 // 1行目に記載している 'use strict' は削除しないでください
 
+function fileOpen() {
+  window.open("document/Presen.pdf", "_blank");
+}
+
 let locaChk = "出社";               //ロケーション確認
 let bTempChk = "問題なし";           //体温確認
 let bConditionChk = "問題なし";      //体調確認
@@ -57,95 +61,88 @@ function createStartMsg() {
   }
 }
 
-const axios = require("axios");
-const URL = `https://toyotajp.webhook.office.com/webhookb2/b19bc3da-933e-4410-9b7e-6d6857c24883@d1c1335e-f582-42a9-b6fe-5e1a16eb9bc8/IncomingWebhook/0342e9a70ec9474f8a6eab985e921fc2/4c970065-e29d-4300-8066-525204717424`;
+function sendToTeams() {
+  const sendToStartWork = new XMLHttpRequest();
+  origin
+  const URL = "https://toyotajp.webhook.office.com/webhookb2/b19bc3da-933e-4410-9b7e-6d6857c24883@d1c1335e-f582-42a9-b6fe-5e1a16eb9bc8/IncomingWebhook/35e6d84b33994d0abc19578c5f647e5e/4c970065-e29d-4300-8066-525204717424";
 
-// axios.post(URL, {
-//   "@type": "MessageCard",
-//   "@context": "https://hogehoge.com/hogehoge.png",
-//   "themeColor": "0076D7",
-//   "summary": "ディスカッション",
-//   "sections": [{
-//       "activityTitle": "Larry Bryant created a new task",
-//       "activitySubtitle": "On Project Tango",
-//       "activityImage": "",
+  sendToStartWork.open("POST", URL);
+  sendToStartWork.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  sendToStartWork.send(
+    {
+      "type": "message",
+      "attachments": [
+        {
+          "contentType": "application/vnd.microsoft.card.adaptive",
+          "contentUrl": null,
+          "content": {
+            "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+            "type": "AdaptiveCard",
+            "version": "1.2",
+            "body": [
+              {
+                "type": "TextBlock",
+                "text": "勤務報告",
+                "wrap": true,
+                "size": "ExtraLarge"
+              }
+            ]
+          }
+        }
+      ]
+    });
 
-//       "facts": [{
-//           "name": "投稿者",
-//           "value": "眞野"
-//       },
-//       {
-//           "name": "URL",
-//           "value": ""
-//       }, {
-//           "name": "Status",
-//           "value": "Not started"
-//       }],
-//       "markdown": true
-//   }]
-// })
-//   .then(res => console.log(res.data));
+  sendToStartWork.onreadystatechange = function () {
+    console.log(sendToStartWork.responseText);
+  }
 
-// axios.post();
+}
 
 function teamsChatSubmit() {
-  const addressAll = [];
+  const mailWithId = [];
   let sendAddress = "";
   let submitMsg = document.getElementById("submitMsg").value;
   // console.log(submitMsg);
 
-  let mail1 = document.getElementById("mail1");
-  let mail2 = document.getElementById("mail2");
-  let mail3 = document.getElementById("mail3");
-  let mail4 = document.getElementById("mail4");
-  let mail5 = document.getElementById("mail5");
-  // console.log("mail1 is", mail1);
+  mailWithId.push(document.getElementById("mail1"));
+  mailWithId.push(document.getElementById("mail2"));
+  mailWithId.push(document.getElementById("mail3"));
+  mailWithId.push(document.getElementById("mail4"));
+  mailWithId.push(document.getElementById("mail5"));
+  
+  const mailAll = mailWithId.map(mail => mail.value);
+  
+  const mailsAfterFilter = mailAll.filter(mail => mail !== "");
+  console.log("mailALL is ", mailsAfterFilter[0]);
 
-  addressAll.push(mail1.value);
-  addressAll.push(mail2.value);
-  addressAll.push(mail3.value);
-  addressAll.push(mail4.value);
-  addressAll.push(mail5.value);
-  // console.log("addressAll is", addressAll);
+  if (mailsAfterFilter[0] === undefined) {
+    return alert("メールアドレスが入力されていません。")
+  }
 
-  let blank = 0;
-
-  for (let i = 0; i < addressAll.length; i++) {
-    if (addressAll[i] !== "") {
-      // console.log("addressAll[i]", addressAll[i]);
-      if (addressAll[i].includes("@mail.toyota.co.jp") === true ||
-        addressAll[i].includes("@tmc.twfr.toyota.co.jp") === true ||
-        addressAll[i].includes("@lexus-int.com") === true) {
-      } else {
-        return alert("トヨタドメイン以外が指定されていませんか。送信相手を確認ください");
-      }
+  for (let i = 0; i < mailsAfterFilter.length; i++) {
+    if (mailsAfterFilter[i].includes("@mail.toyota.co.jp") === true ||
+      mailsAfterFilter[i].includes("@tmc.twfr.toyota.co.jp") === true ||
+      mailsAfterFilter[i].includes("@lexus-int.com") === true) {
     } else {
-      // console.log(blank);
-      blank += 1;
-    }
-    // console.log(blank);
-    if (blank === 5) {
-      return alert("メールアドレスが入力されていません。")
+      return alert("トヨタドメイン以外が指定されていませんか。送信相手を確認ください");
     }
   }
 
-  for (let j = 0; j < addressAll.length; j++) {
+  for (let j = 0; j < mailsAfterFilter.length; j++) {
     if (j === 0) {
-      sendAddress = addressAll[0];
-    } else if (j > 0 && addressAll[j] !== "") {
-      sendAddress = `${sendAddress},${addressAll[j]}`;
+      sendAddress = mailsAfterFilter[0];
+    } else {
+      sendAddress = `${sendAddress},${mailsAfterFilter[j]}`;
     }
   }
   // console.log("sendAddress is :", sendAddress);
-  window.open(`https://teams.microsoft.com/l/chat/0/0?users=${addressAll}&message=${submitMsg}`, "_blank");
+  window.open(`https://teams.microsoft.com/l/chat/0/0?users=${mailsAfterFilter}&message=${submitMsg}`, "_blank");
 }
-
+// 個別チャット送信
+// https://teams.microsoft.com/l/chat/0/0?users=***&message=***
 
 function timeMngSystem() {
   window.open("https://kintai.kitora.toyota.co.jp/JNET2/start", "_blank");
   document.getElementById("WRKZMENU_H2WKDLLINK").click();
   // href = "javascript:submitAction_win2(document.win2,'WRKZMENU_H2WKDLLINK');"
 }
-
-// 個別チャット送信
-// https://teams.microsoft.com/l/chat/0/0?users=***&message=***
